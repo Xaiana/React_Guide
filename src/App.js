5aaa30fe8438c8839eaf2053750086ece7bcd222
Melissa Keith <MelissaKeith@simplymacs-MacBook-Air.local>
2016-12-01 20:06:22 -0800

    addWeek method works and changed week components render method

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import './App.css';
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.weekTemplate = { links: [{description: '', url: ''}] };
     this.state = {
       descriptionValue: '',
       urlValue: '',
@@ -15,44 +16,47 @@ class App extends React.Component {
           { links: [{description: 'anything', url: 'www.google.com'}] },
           { links: [{description: 'anything', url: 'www.google.com'}] }
         ]
-      }
+      };
       this.addLink = this.addLink.bind(this);
       this.addWeek = this.addWeek.bind(this);
       this.renderWeeks = this.renderWeeks.bind(this);
     }
 
-  addWeek(event) {
-    const resources = this.state.weeks;
-    const newWeekResource = resources.push({}); //add empty week object to this.state.weeks array
-    this.renderWeeks(newWeekResource, resources);
+  addWeek() {
+    const weeks = this.state.weeks.slice(0); // point in weeks array
+    weeks.push(this.weekTemplate); //add empty week object
+    this.setState({weeks});
   }
 
   addLink(resource, destination) { //resource is the package containing the new link object pair, destination is the week id to add to.
-    const _destination = this.state[destination]; //[destination] is in brackets because it's value is a string
-    _destination.push(resource) ;
-    this.setState({[destination] : _destination});
+    const _destination = this.state.weeks[destination].links; //[destination] is in brackets because it's value is a string
+    _destination.push(resource); // add the resource to links object's array
+    this.setState({destination: _destination}); // update state to show added link; destination is the week represented by links to add too _destination is location that accepted resource package
   }
 
-  renderWeeks(newWeekResource, destination) {
-    return this.state.weeks.map((week, idx) => {
+  renderWeeks() {
+    return this.state.weeks.map((week, i) => {
       return <Week
-              key={idx}
-              links={week.links}
-              addLink={this.addLink}
-              title={'Week ' + (idx + 1)}
+              key={i+week}
+              id={i}
+              title={'Week ' + (i + 1)}
+              week={this.state.weeks[i].links}
               addLink={this.addLink}
             />;
     });
   }
 
   render() {
+    const nextWeek = this.state.weeks.length + 1;
     return (
       <div>
         <Header header={"React Journal"} />
-        <form renderWeeks={this.renderWeeks}>
-        <Week />
-        <button>Submit</button>
-        </form>
+        <button onClick={this.addWeek}> {/*addWeek is callback function*/}
+          AddWeek
+        </button>
+        <div>
+          {this.renderWeeks()}
+        </div>
       </div>
     );
   }
--- src/Week.js
+++ src/Week.js
@@ -18,20 +18,24 @@ export default class Week extends React.Component {
 
   handleSubmit(event) {
     event.preventDefault();
-    const newResource = Object.assign({}, this.state); // create object and assign updated state for each time a link is added
-    this.props.addLink(newResource, this.props.key) //assigns the newResource package to
+    const newResource = Object.assign({}, this.state); // create object and assign updated state to it each time a link is entered in form
+    this.props.addLink(newResource, this.props.id) //assigns the newResource package to a week represented by key index
 
   }
 
   render() {
-    const weekMap = this.props.weeks[i].links.map((linkItem, i) => {
-      return <li key={i}> <a href={linkItem.url}> {linkItem.description}</a></li>
-    })
+    const weekMap = this.props.week.map((linkItem, i) => {
+      return linkItem.url && linkItem.description && // condition statements only run if there is already a link and description
+      <li key={i}> <a href={linkItem.url}> {linkItem.description}</a></li>
+    });
 
     return(
       <div>
         <h2>{this.props.title}</h2>
-        <ul>{weekMap}</ul>
+        {
+          weekMap.length > 0 && // if weekMap has length greater than 0 then render weekMap
+          <ul>{weekMap}</ul>
+        }
         <form onSubmit={this.handleSubmit}>
           <input name={'description'} value={this.state.description} onChange={this.handleChange} />
           <input name={'url'} value={this.state.url} onChange={this.handleChange} />
